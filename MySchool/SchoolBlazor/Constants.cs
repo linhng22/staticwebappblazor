51260b5d34b65160cd67b0218349ecb24c04b984
linhng2210 <k.linh2210@gmail.com>
2024-02-20 09:53:02 -0800

    first commit

---
+++ MySchool/SchoolBlazor/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace SchoolBlazor;
+
+public class Constants
+{
+    // public const string BASE_URL = "https://functions4870mze.azurewebsites.net/";
+    public const string BASE_URL = "/";
+}
